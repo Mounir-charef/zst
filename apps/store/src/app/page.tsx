18d7c74bf359f71effed5c1b7111550ec8cf5a12
Mounir <polomnr1@gmail.com>
2024-02-07 10:23:34 +0100

    add: store portal

---
+++ apps/store/src/app/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="font-bold text-center">
+      <h1 className="text-4xl">Hello World from store</h1>
+    </div>
+  );
+};
+
+export default page;
