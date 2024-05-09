b51a44b7880564af77c4dcdde52b59ad9aedae41
Mounir <polomnr1@gmail.com>
2024-05-09 14:03:52 +0100

    Add Dashboard Card varients

---
+++ apps/store/src/components/dashboard/DashboardActionCard.tsx
@@ -0,0 +1,35 @@
+import {
+  Button,
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@mono/ui';
+import { ReactNode, memo } from 'react';
+
+interface DashboardActionCardProps {
+  title: string;
+  description: string;
+  action: ReactNode;
+}
+
+const DashboardActionCard = ({
+  action,
+  description,
+  title,
+}: DashboardActionCardProps) => {
+  return (
+    <Card className="sm:col-span-2">
+      <CardHeader className="pb-3">
+        <CardTitle>{title}</CardTitle>
+        <CardDescription className="max-w-lg text-balance leading-relaxed">
+          {description}
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>{action}</CardFooter>
+    </Card>
+  );
+};
+
+export default memo(DashboardActionCard);
