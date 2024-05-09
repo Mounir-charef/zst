94aa2899855bad3d77ddb2427fbc8073dc418789
Mounir <polomnr1@gmail.com>
2024-05-09 16:18:55 +0100

    add session provider and optimize providers promises resolve

--- apps/admin/src/app/[locale]/layout.tsx
+++ apps/admin/src/app/[locale]/layout.tsx
@@ -23,10 +23,13 @@ export default async function RootLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  const messages = await getMessages();
-  const timezone = await getTimeZone();
+  const [messages, timezone, session] = await Promise.all([
+    getMessages(),
+    getTimeZone(),
+    getAuthSession(),
+  ]);
+
   const { langDir, hrefLang } = availableLocalesMap[locale] || defaultLocale;
-  const session = await getAuthSession();
   return (
     <html
       lang={hrefLang}
