2a184f6294ee401bc8644c3f6712f2c2ee13ba0c
Mounir <polomnr1@gmail.com>
2024-05-09 08:48:01 +0100

    add a draft toolbar for itemtable variant

--- libs/ui-kit/ui/src/lib/components/client-table/ItemTableToolbar.tsx
+++ libs/ui-kit/ui/src/lib/components/client-table/ItemTableToolbar.tsx
@@ -1,14 +1,11 @@
 'use client';
 
 import { Table } from '@tanstack/react-table';
-import { X } from 'lucide-react';
 import { Button } from '../../ui/button';
-import {
-  DataTableFacetedFilter,
-  DataTableFacetedFilterProps,
-} from './FacetedFilter';
-import { DataTableViewOptions } from './ViewOptions';
 import { ToggleGroup, ToggleGroupItem } from '../../ui/toggle-group';
+import { DataTableFacetedFilterProps } from './FacetedFilter';
+import { DataTableViewOptions } from './ViewOptions';
+import { useState } from 'react';
 
 interface ItemTableToolbarProps<TData, TValue> {
   table: Table<TData>;
@@ -23,50 +20,31 @@ export function ItemTableToolbar<TData, TValue>({
   table,
   filterOptions,
 }: ItemTableToolbarProps<TData, TValue>) {
-  const isFiltered = table.getState().columnFilters.length > 0;
+  const [selectedFilter, setSelectedFilter] = useState<string>('all');
   return (
     <div className="flex items-start justify-between gap-x-4">
       <div className="flex flex-1 flex-wrap items-center gap-1 gap-x-2">
-        {/* {filterOptions?.map((filterOption) => {
-          if (!filterOption.column) return null;
-          return (
-            <DataTableFacetedFilter
-              key={filterOption.title}
-              {...filterOption}
-            />
-          );
-        })} */}
         <ToggleGroup
-          type="single"
           defaultValue="all"
-          className="bg-muted text-muted-foreground inline-flex h-10 items-center justify-center rounded-md p-1"
+          variant="filter"
+          size="sm"
+          type="single"
+          className="bg-muted text-muted-foreground rounded-md p-1"
+          onValueChange={(value) => {
+            if (!!value) setSelectedFilter(value);
+          }}
+          value={selectedFilter}
         >
-          <ToggleGroupItem
-            className="ring-offset-background focus-visible:ring-ring aria-checked:bg-background aria-checked:text-foreground inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 aria-checked:shadow-sm"
-            value="all"
-            aria-label="Toggle All"
-          >
+          <ToggleGroupItem value="all" aria-label="Toggle All">
             All
           </ToggleGroupItem>
-          <ToggleGroupItem
-            className="ring-offset-background focus-visible:ring-ring aria-checked:bg-background aria-checked:text-foreground inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 aria-checked:shadow-sm"
-            value="low"
-            aria-label="Toggle low"
-          >
+          <ToggleGroupItem value="low" aria-label="Toggle low">
             Low
           </ToggleGroupItem>
-          <ToggleGroupItem
-            className="ring-offset-background focus-visible:ring-ring aria-checked:bg-background aria-checked:text-foreground inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 aria-checked:shadow-sm"
-            value="medium"
-            aria-label="Toggle medium"
-          >
+          <ToggleGroupItem value="medium" aria-label="Toggle medium">
             Medium
           </ToggleGroupItem>
-          <ToggleGroupItem
-            className="ring-offset-background focus-visible:ring-ring aria-checked:bg-background aria-checked:text-foreground inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 aria-checked:shadow-sm"
-            value="high"
-            aria-label="Toggle high"
-          >
+          <ToggleGroupItem value="high" aria-label="Toggle high">
             High
           </ToggleGroupItem>
         </ToggleGroup>
