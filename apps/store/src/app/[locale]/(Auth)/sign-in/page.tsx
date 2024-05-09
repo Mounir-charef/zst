60c4f7de9e3c646af9f957d6d3349fcf87e8a815
Mounir <polomnr1@gmail.com>
2024-05-09 16:40:48 +0100

    add a draft sign-in page

--- apps/store/src/app/[locale]/(Auth)/sign-in/page.tsx
+++ apps/store/src/app/[locale]/(Auth)/sign-in/page.tsx
@@ -1,27 +1,38 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@mono/ui';
+import { Button, Input, Label } from '@mono/ui';
+import Image from 'next/image';
 import { memo } from 'react';
+import { Link } from '../../../../navigation';
 import SignInForm from '../_components/SignInForm';
 
 const page = () => {
   return (
-    <div className="bg-accent grid h-full place-items-center">
-      <Card className="mx-auto w-80 shadow sm:w-96">
-        <CardHeader className="text-center">
-          <CardTitle className="text-2xl">Sign in</CardTitle>
-          <CardDescription className="text-muted-foreground italic">
-            Sign in to dashboard
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-8">
+    <div className="h-full w-full lg:grid lg:grid-cols-2">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-muted-foreground text-balance">
+              Enter your email below to login to your account
+            </p>
+          </div>
           <SignInForm />
-        </CardContent>
-      </Card>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{' '}
+            <Link href="#" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted hidden lg:block">
+        <Image
+          src="/placeholder.svg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
     </div>
   );
 };
