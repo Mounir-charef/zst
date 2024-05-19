2ef63393f9cfa3608ce42dd290148241158f4d0e
Mounir <polomnr1@gmail.com>
2024-05-19 09:48:19 +0100

    add mobile navbar (without navigation)

--- apps/store/src/components/navbar/NavBar.tsx
+++ apps/store/src/components/navbar/NavBar.tsx
@@ -10,7 +10,7 @@ import UserNav from './UserNav';
 const NavBar = () => {
   return (
     <div className="bg-background text-foreground border-border dark fixed top-0 z-50 w-full border-b">
-      <div className="mx-auto hidden h-20 w-full max-w-[1700px] items-center justify-between gap-x-6 p-4 sm:flex">
+      <div className="mx-auto hidden h-20 w-full max-w-[1700px] items-center justify-between gap-x-6 p-4 md:flex">
         <nav className="flex items-center gap-6">
           <Link
             href="/"
@@ -36,6 +36,27 @@ const NavBar = () => {
           <Notification />
           <UserNav />
         </div>
+      </div>
+
+      {/* mobile navbar */}
+      <div className="flex items-center justify-between p-4 md:hidden">
+        <Link
+          href="/"
+          aria-label="home"
+          className={buttonVariants({
+            variant: 'ghost',
+          })}
+        >
+          <Image
+            src="/Brand.png"
+            width={80}
+            height={80}
+            alt="Brand"
+            priority
+            quality={100}
+          />
+        </Link>
+        <UserNav />
       </div>
     </div>
   );
