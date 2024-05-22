291fa70212cfee6aa4afb86c22bb473175c516c1
Mounir <polomnr1@gmail.com>
2024-05-22 17:38:53 +0100

    fix backroung

--- apps/store/src/app/[locale]/(main)/layout.tsx
+++ apps/store/src/app/[locale]/(main)/layout.tsx
@@ -12,7 +12,7 @@ export default async function RootLayout({
     redirect('/sign-in');
   }
   return (
-    <main className="bg-accent flex min-h-full w-full flex-col pt-20">
+    <main className="bg-muted/40 flex min-h-full w-full flex-col pt-20">
       <NavBar />
       <div className="mx-auto w-full max-w-7xl flex-grow flex-col px-2 py-12 sm:py-6">
         {children}
