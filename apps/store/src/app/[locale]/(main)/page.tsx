a280401c870294f8d95008fa3100f902175db589
Mounir <polomnr1@gmail.com>
2024-05-29 11:30:23 +0100

    add sales to dashboard

--- apps/store/src/app/[locale]/(main)/page.tsx
+++ apps/store/src/app/[locale]/(main)/page.tsx
@@ -1,8 +1,4 @@
 import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-  Button,
   Card,
   CardContent,
   CardDescription,
@@ -18,10 +14,11 @@ import {
   Loader2,
   Users,
 } from 'lucide-react';
-import InfoCard from '../../../components/cards/InfoCard';
-import RecentTransactionsTable from './_components/RecentTransactionsTable';
-import { Link } from '../../../navigation';
 import { Suspense } from 'react';
+import InfoCard from '../../../components/cards/InfoCard';
+import { Link } from '../../../navigation';
+import RecentTransactionsTable from './_components/RecentTransactionsTable';
+import Sales from './_components/Sales';
 
 export default async function Dashboard() {
   return (
@@ -89,76 +86,17 @@ export default async function Dashboard() {
         <Card className="h-fit xl:col-span-2">
           <CardHeader>
             <CardTitle>Recent Sales</CardTitle>
+            <CardDescription>Recent sales from your store.</CardDescription>
           </CardHeader>
-          <CardContent className="grid gap-8">
-            <div className="flex items-center gap-4">
-              <Avatar className="hidden h-9 w-9 sm:flex">
-                <AvatarImage src="/avatars/01.png" alt="Avatar" />
-                <AvatarFallback>OM</AvatarFallback>
-              </Avatar>
-              <div className="grid gap-1">
-                <p className="text-sm font-medium leading-none">
-                  Olivia Martin
-                </p>
-                <p className="text-muted-foreground text-sm">
-                  olivia.martin@email.com
-                </p>
-              </div>
-              <div className="ml-auto font-medium">+$1,999.00</div>
-            </div>
-            <div className="flex items-center gap-4">
-              <Avatar className="hidden h-9 w-9 sm:flex">
-                <AvatarImage src="/avatars/02.png" alt="Avatar" />
-                <AvatarFallback>JL</AvatarFallback>
-              </Avatar>
-              <div className="grid gap-1">
-                <p className="text-sm font-medium leading-none">Jackson Lee</p>
-                <p className="text-muted-foreground text-sm">
-                  jackson.lee@email.com
-                </p>
-              </div>
-              <div className="ml-auto font-medium">+$39.00</div>
-            </div>
-            <div className="flex items-center gap-4">
-              <Avatar className="hidden h-9 w-9 sm:flex">
-                <AvatarImage src="/avatars/03.png" alt="Avatar" />
-                <AvatarFallback>IN</AvatarFallback>
-              </Avatar>
-              <div className="grid gap-1">
-                <p className="text-sm font-medium leading-none">
-                  Isabella Nguyen
-                </p>
-                <p className="text-muted-foreground text-sm">
-                  isabella.nguyen@email.com
-                </p>
-              </div>
-              <div className="ml-auto font-medium">+$299.00</div>
-            </div>
-            <div className="flex items-center gap-4">
-              <Avatar className="hidden h-9 w-9 sm:flex">
-                <AvatarImage src="/avatars/04.png" alt="Avatar" />
-                <AvatarFallback>WK</AvatarFallback>
-              </Avatar>
-              <div className="grid gap-1">
-                <p className="text-sm font-medium leading-none">William Kim</p>
-                <p className="text-muted-foreground text-sm">will@email.com</p>
-              </div>
-              <div className="ml-auto font-medium">+$99.00</div>
-            </div>
-            <div className="flex items-center gap-4">
-              <Avatar className="hidden h-9 w-9 sm:flex">
-                <AvatarImage src="/avatars/05.png" alt="Avatar" />
-                <AvatarFallback>SD</AvatarFallback>
-              </Avatar>
-              <div className="grid gap-1">
-                <p className="text-sm font-medium leading-none">Sofia Davis</p>
-                <p className="text-muted-foreground text-sm">
-                  sofia.davis@email.com
-                </p>
-              </div>
-              <div className="ml-auto font-medium">+$39.00</div>
-            </div>
-          </CardContent>
+          <Suspense
+            fallback={
+              <CardContent className="flex h-44 items-center justify-center">
+                <Loader2 className="text-primary h-8 w-8 animate-spin" />
+              </CardContent>
+            }
+          >
+            <Sales />
+          </Suspense>
         </Card>
       </div>
     </main>
